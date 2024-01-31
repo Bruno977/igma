19f8b230d8620694831faa591a77faee5b1675bc
Bruno Rodrigues <bruno_151997@hotmail.com>
2024-01-31 19:01:35 -0300

    feat: configure prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,19 +5,17 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
-      {
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": "error"
-      }
-      
     ],
+    'prettier/prettier': 'error',
+
   },
 }
