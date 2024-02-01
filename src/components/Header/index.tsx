8ade10206d815bbbf5dcbfeacc0e4a2c44a9120a
Bruno Rodrigues <bruno_151997@hotmail.com>
2024-01-31 23:52:48 -0300

    feat: update header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
+import { IconUser } from '../../assets/icons/IconUser';
+import { LogoHeader } from '../../assets/icons/LogoHeader';
+import { HeaderContainer, Button } from './style';
+
 export function Header() {
-  return <header>header</header>;
+  return (
+    <HeaderContainer>
+      <LogoHeader />
+      <Button type="button">
+        <IconUser />
+      </Button>
+    </HeaderContainer>
+  );
 }
